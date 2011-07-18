ebb72d094a0ce3ac4a6be26f12dd375f6287ef52
Guilherme Gonçalves <guilherme.p.gonc@gmail.com>
2011-07-18 09:41:25 -0300

    Replaced sys.path tampering with relative imports.

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,7 +1,4 @@
-import sys
 import importlib
-
-sys.path.append("commands")
 
 command_modules = [
     "batima",
@@ -14,5 +11,5 @@ command_modules = [
 
 commands = []
 for cmd in command_modules:
-	mod = importlib.import_module(cmd, "commands")
+	mod = importlib.import_module("." + cmd, "commands")
 	commands.extend(mod.command_description)
